aec843dbb1b754578dbb04361c5f0103128ae17b
John Henrie <gimmeyerjunkmail@gmail.com>
2023-10-24 09:12:24 -0600

    first groovy code

---
+++ first.groovy
@@ -0,0 +1,11 @@
+def String x = "hi"
+println x
+println(Byte.MIN_VALUE)
+println(Byte.MAX_VALUE)
+println(Integer.MIN_VALUE)
+println(Integer.MAX_VALUE)
+long y = (long)-1*Integer.MIN_VALUE
+println(Math.log(y)/Math.log(2))
+x = 20
+println x
+print 'Hello world!'
